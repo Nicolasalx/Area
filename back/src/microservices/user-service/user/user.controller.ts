581fe26780a3479c0d80ff242c6d0695b146f9b6
NicolasAlx <nicolas1.alexandre@epitech.eu>
2024-12-05 11:43:13 +0100

    fix: update source files with correct table namming

--- back/src/microservices/user-service/user/user.controller.ts
+++ back/src/microservices/user-service/user/user.controller.ts
@@ -1,25 +1,25 @@
 import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
-import { User } from '@prisma/client';
+import { Users } from '@prisma/client';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  async getUsers(): Promise<User[]> {
+  async getUsers(): Promise<Users[]> {
     return this.userService.getUsers();
   }
 
   @Get(':id')
-  async getUserById(@Param('id') id: number): Promise<User> {
+  async getUserById(@Param('id') id: string): Promise<Users> {
     return this.userService.getUserById(id);
   }
 
   @Post()
   async createUser(
     @Body() body: { username: string; email: string; password: string },
-  ): Promise<User> {
+  ): Promise<Users> {
     return this.userService.createUser(
       body.username,
       body.email,
