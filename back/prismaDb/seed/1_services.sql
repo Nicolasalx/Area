2a4b60f3760c1621bced725514433493c28d1be1
Nicolas Alexandre <114945623+Nicolasalx@users.noreply.github.com>
2024-12-05 14:22:42 +0100

    feat(migration): create migration scripts for database (#54)
    * feat(migration): create migration scripts for database
    
    * fix: lint for Javascript file migrate.js has been fixed
    
    * fix(database): update migration script for the database

---
+++ back/prismaDb/seed/1_services.sql
@@ -0,0 +1,5 @@
+INSERT INTO "Services" ("name", "description", "isActive", "createdAt")
+VALUES
+    ('Google', 'Google services like Gmail, Calendar, Drive, etc.', true, NOW()),
+    ('Spotify', 'Spotify music streaming service.', true, NOW()),
+    ('Dropbox', 'Dropbox cloud storage service.', true, NOW());
