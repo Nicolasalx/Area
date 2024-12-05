b2f9780ef24210fc57e8da0960ad89a221449e11
NicolasAlx <nicolas1.alexandre@epitech.eu>
2024-12-05 16:24:14 +0100

    feat(workflow): create endpoint to create and retrieve workflows

---
+++ back/src/common/interfaces/action.interface.ts
@@ -0,0 +1,5 @@
+export interface Action {
+  name: string;
+  service: string;
+  token: string;
+}
---
+++ back/src/common/interfaces/workflow.interface.ts
@@ -0,0 +1,9 @@
+import { Action } from "./action.interface";
+import { Reaction } from "./reaction.interface";
+
+export interface WorkflowDto {
+  name: string;
+  sessionId: string;
+  actions: Action[];
+  reactions: Reaction[];
+}
