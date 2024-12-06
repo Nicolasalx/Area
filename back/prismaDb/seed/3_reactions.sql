3c5c2352942eae12b1ab1a669979ffe43dce10be
Nicolas Alexandre <114945623+Nicolasalx@users.noreply.github.com>
2024-12-06 15:24:20 +0100

    feat: reaction has been added to create a directory in drive (#69)

--- back/prismaDb/seed/3_reactions.sql
+++ back/prismaDb/seed/3_reactions.sql
@@ -3,5 +3,4 @@ VALUES
     ('send_email', 'Sends an email when triggered.', '{"reaction": "send_email"}', true, NOW(), (SELECT id FROM "Services" WHERE "name" = 'google' LIMIT 1)),
     ('set_calendar_event', 'Sets an event in Google Calendar when triggered.', '{"reaction": "set_event_calendar"}', true, NOW(), (SELECT id FROM "Services" WHERE "name" = 'google' LIMIT 1)),
     ('create_task', 'Create a task in Google Task when triggered.', '{"reaction": "create_task"}', true, NOW(), (SELECT id FROM "Services" WHERE "name" = 'google' LIMIT 1)),
-    ('create_drive_file', 'Create a file in Google Drive when triggered.', '{"reaction": "create_drive_file"}', true, NOW(), (SELECT id FROM "Services" WHERE "name" = 'google' LIMIT 1));
-
+    ('create_drive_element', 'Create an element (it can be a folder, forms, docs, spreadsheet) in Google Drive when triggered.', '{"reaction": "create_drive_element"}', true, NOW(), (SELECT id FROM "Services" WHERE "name" = 'google' LIMIT 1));
