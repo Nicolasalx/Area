5d474f19de17b39041cb8ee47dbf5392320e59b1
Tiphaine Bertone <tiphaine.bertone@epitech.eu>
2024-12-07 15:12:52 +0100

    style(front): remove useless fonction and correct command in doc

--- front/app/api/auth/google/callback/route.ts
+++ front/app/api/auth/google/callback/route.ts
@@ -2,52 +2,6 @@ import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import axios from "axios";
 
-async function getGoogleOAuthTokens(code: string) {
-  const url = "https://oauth2.googleapis.com/token";
-  const values = {
-    code,
-    client_id: process.env.GOOGLE_CLIENT_ID!,
-    client_secret: process.env.GOOGLE_CLIENT_SECRET!,
-    redirect_uri: process.env.GOOGLE_REDIRECT_URI!,
-    grant_type: "authorization_code",
-  };
-
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(values),
-    });
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error getting Google OAuth tokens:", error);
-    throw new Error("Failed to get Google OAuth tokens");
-  }
-}
-
-async function getGoogleUser(access_token: string /*, id_token: string*/) {
-  try {
-    const response = await fetch(
-      "https://www.googleapis.com/oauth2/v2/userinfo",
-      {
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-        },
-      },
-    );
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error getting Google user:", error);
-    throw new Error("Failed to get Google user");
-  }
-}
-
 export async function GET(request: Request) {
   const searchParams = new URL(request.url).searchParams;
   const code = searchParams.get("code");
@@ -68,7 +22,6 @@ export async function GET(request: Request) {
     sameSite: "lax",
     maxAge: 60 * 60 * 24 * 7, // 1 week
   });
-  console.log(data);
   // Return HTML that sends a message to the opener window
   return new NextResponse(
     `
