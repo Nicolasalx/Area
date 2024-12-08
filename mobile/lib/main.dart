d088c68f2ae9ae0060488e614f13e155f7667766
Nicolas Melet <nicolas.melet@epitech.eu>
2024-12-08 23:21:13 +0100

    feat: login page ui completed

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 
 void main() {
@@ -82,15 +81,17 @@ class _MyHomePageState extends State<MyHomePage> {
     final formkey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
-          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title,
-              style:
-                  const TextStyle(fontWeight: FontWeight.w700, fontSize: 30))),
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(
+          widget.title,
+          style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 30),
+        ),
+      ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -118,183 +119,202 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.only(top: 50.0, left: 20, right: 20),
+          padding: const EdgeInsets.only(top: 100.0, left: 20, right: 20),
           child: Container(
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: const Color.fromARGB(255, 255, 255, 255),
-                border: Border.all(
-                    color: const Color.fromARGB(255, 185, 185, 185))),
+              borderRadius: BorderRadius.circular(10),
+              color: const Color.fromARGB(255, 255, 255, 255),
+              border: Border.all(
+                color: const Color.fromARGB(255, 185, 185, 185),
+              ),
+            ),
             child: Column(
               children: <Widget>[
                 const Padding(
                   padding: EdgeInsets.only(top: 30.0),
                   child: Center(
-                    child: Text('Welcome',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w700, fontSize: 30)),
+                    child: Text(
+                      'Welcome',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 30,
+                      ),
+                    ),
                   ),
                 ),
                 const Padding(
                   padding: EdgeInsets.only(top: 5.0),
                   child: Center(
-                    child: Text('Sign in to your account',
-                        style: TextStyle(fontSize: 15)),
+                    child: Text(
+                      'Sign in to your account',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Color.fromARGB(255, 119, 119, 119),
+                      ),
+                    ),
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15),
-                  child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Form(
-                        key: formkey,
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Padding(
-                                  padding: const EdgeInsets.all(12.0),
-                                  child: TextFormField(
-                                      validator: MultiValidator([
-                                        RequiredValidator(
-                                            errorText: 'Enter email address'),
-                                        EmailValidator(
-                                            errorText:
-                                                'Please correct email filled'),
-                                      ]),
-                                      decoration: InputDecoration(
-                                          hintText: 'Email',
-                                          labelText: 'Email',
-                                          prefixIcon: Icon(
-                                            Icons.email,
-                                            //color: Colors.green,
-                                          ),
-                                          errorStyle: TextStyle(fontSize: 18.0),
-                                          border: OutlineInputBorder(
-                                              borderSide:
-                                                  BorderSide(color: Colors.red),
-                                              borderRadius: BorderRadius.all(
-                                                  Radius.circular(9.0)))))),
-                              Padding(
-                                padding: const EdgeInsets.all(12.0),
-                                child: TextFormField(
-                                  validator: MultiValidator([
-                                    RequiredValidator(
-                                        errorText: 'Please enter Password'),
-                                    MinLengthValidator(8,
-                                        errorText:
-                                            'Password must be atlist 8 digit'),
-                                    PatternValidator(r'(?=.*?[#!@$%^&*-])',
-                                        errorText:
-                                            'Password must be atlist one special character')
-                                  ]),
-                                  decoration: InputDecoration(
-                                    hintText: 'Password',
-                                    labelText: 'Password',
-                                    prefixIcon: Icon(
-                                      Icons.key,
-                                      color: Colors.green,
-                                    ),
-                                    errorStyle: TextStyle(fontSize: 18.0),
-                                    border: OutlineInputBorder(
-                                        borderSide:
-                                            BorderSide(color: Colors.red),
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(9.0))),
-                                  ),
+                  padding: const EdgeInsets.all(12.0),
+                  child: Form(
+                    key: formkey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const Padding(
+                          padding: EdgeInsets.only(left: 15),
+                          child: Text(
+                            'Email',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: TextFormField(
+                            validator: MultiValidator([
+                              RequiredValidator(errorText: 'Email is required'),
+                              EmailValidator(
+                                  errorText: 'Invalid email address'),
+                            ]).call,
+                            decoration: const InputDecoration(
+                              hintText: 'Enter your email',
+                              prefixIcon: Icon(
+                                Icons.alternate_email,
+                                color: Color.fromARGB(255, 119, 119, 119),
+                              ),
+                              errorStyle: TextStyle(fontSize: 18.0),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(9.0),
                                 ),
                               ),
-                              Container(
-                                margin: EdgeInsets.fromLTRB(180, 0, 0, 0),
-                                child: Text('Forget Password!'),
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 15),
+                          child: Text(
+                            'Password',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: 'Enter your password',
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: Color.fromARGB(255, 119, 119, 119),
                               ),
-                              Padding(
-                                padding: const EdgeInsets.all(28.0),
-                                child: Container(
-                                  child: ElevatedButton(
-                                      style: ButtonStyle(
-                                          backgroundColor:
-                                              MaterialStateProperty.all(
-                                                  Colors.green),
-                                          padding: MaterialStateProperty.all(
-                                              const EdgeInsets.all(20)),
-                                          textStyle: MaterialStateProperty.all(
-                                              const TextStyle(
-                                                  fontSize: 14,
-                                                  color: Colors.white))),
-                                      onPressed: () {
-                                        if (formkey.currentState!.validate()) {
-                                          print('form submiitted');
-                                        }
-                                      },
-                                      child: const Text(
-                                        'Login',
-                                        style: TextStyle(
-                                            color: Colors.white, fontSize: 22),
-                                      )),
+                              errorStyle: TextStyle(fontSize: 18.0),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(9.0),
                                 ),
                               ),
-                              Center(
-                                child: Padding(
-                                  padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
-                                  child: Center(
-                                    child: Text(
-                                      'Or Sign In Using!',
-                                      style: TextStyle(
-                                          fontSize: 18, color: Colors.black),
-                                    ),
+                            ),
+                          ),
+                        ),
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 10.0, left: 10, right: 10),
+                            child: SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      WidgetStateProperty.all(Colors.green),
+                                  padding: WidgetStateProperty.all(
+                                    const EdgeInsets.all(20),
+                                  ),
+                                  textStyle: WidgetStateProperty.all(
+                                    const TextStyle(
+                                        fontSize: 14, color: Colors.white),
                                   ),
                                 ),
+                                onPressed: () {
+                                  if (formkey.currentState!.validate()) {
+                                    print('form submited');
+                                  }
+                                },
+                                child: const Text(
+                                  'Sign in',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 22),
+                                ),
                               ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                                    child: Row(
-                                      children: [
-                                        Container(
-                                            height: 40,
-                                            width: 40,
-                                            child: Image.asset(
-                                              'assets/social.jpg',
-                                              fit: BoxFit.cover,
-                                            )),
-                                        Container(
-                                          height: 70,
-                                          width: 70,
-                                          child: Image.asset(
-                                            'assets/vishal.png',
-                                            fit: BoxFit.cover,
-                                          ),
-                                        ),
-                                        Container(
-                                          height: 40,
-                                          width: 40,
-                                          child: Image.asset(
-                                            'assets/google.png',
-                                            fit: BoxFit.cover,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
+                            ),
+                          ),
+                        ),
+                        const Center(
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 20.0),
+                            child: Center(
+                              child: Text(
+                                'Or continue with',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Color.fromARGB(255, 119, 119, 119),
+                                ),
                               ),
-                              Center(
-                                child: Container(
-                                  padding: EdgeInsets.only(top: 50),
-                                  child: Text(
-                                    'SIGN UP!',
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w700,
-                                      color: Colors.lightBlue,
-                                    ),
+                            ),
+                          ),
+                        ),
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 20.0, left: 10, right: 10),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  width: 1,
+                                  color:
+                                      const Color.fromARGB(255, 119, 119, 119),
+                                ),
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              width: double.infinity,
+                              child: ElevatedButton.icon(
+                                label: const Text(
+                                  'Sign in with Google',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 22),
+                                ),
+                                icon: Image.asset(
+                                  'assets/google.png',
+                                  width: 40,
+                                  height: 40,
+                                ),
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      WidgetStateProperty.all(Colors.white),
+                                  padding: WidgetStateProperty.all(
+                                      const EdgeInsets.all(20)),
+                                  textStyle: WidgetStateProperty.all(
+                                    const TextStyle(
+                                        fontSize: 14, color: Colors.white),
                                   ),
                                 ),
-                              )
-                            ]),
-                      )),
+                                onPressed: () {
+                                  print('form submited');
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
