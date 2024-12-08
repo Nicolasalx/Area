65e9f15fb2ff208567fc09109d6b6ff7457dc5ce
thibaudcathala <114906947+thibaudcathala@users.noreply.github.com>
2024-12-08 19:04:07 +0100

    feat: reaction to send discord message (#99)
    * feat: discord reaction to send message using discord webhook
    
    * fix: formating issue in the discord service
    
    * fix: missing declaration of the dicord service in unittest
    
    * feat: add discord reation to the db
    
    * fix: error introduc in the reaction.sql file
    
    * feat: add discord service and reaction to the db

---
+++ back/src/microservices/reaction-service/discord/discord.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+interface DiscordWebhook {
+  url: string;
+  channelId: string;
+  token: string;
+}
+
+@Injectable()
+export class DiscordService {
+  private webhook: DiscordWebhook;
+
+  constructor() {
+    this.webhook = {
+      url: process.env.DISCORD_WEBHOOK_URL,
+      channelId: process.env.DISCORD_CHANNEL_ID,
+      token: process.env.DISCORD_TOKEN,
+    };
+  }
+
+  async handleAction(reaction: string, data: any): Promise<string> {
+    switch (reaction.toLowerCase()) {
+      case 'send_message':
+        return this.sendMessage(data);
+      default:
+        return 'Reaction not recognized for Discord';
+    }
+  }
+
+  private async sendMessage(data: { message: string }): Promise<string> {
+    const { message } = data;
+
+    try {
+      await axios.post(this.webhook.url, {
+        content: message,
+      });
+    } catch (error) {
+      console.error('Error sending message to Discord:', error);
+      return 'Error sending message to Discord';
+    }
+    return 'Message sent';
+  }
+}
--- back/src/microservices/reaction-service/reaction-service.module.ts
+++ back/src/microservices/reaction-service/reaction-service.module.ts
@@ -3,9 +3,10 @@ import { ReactionController } from './reaction/reaction.controller';
 import { ReactionService } from './reaction/reaction.service';
 import { GoogleService } from './google/google.service';
 import { PrismaService } from '@prismaService/prisma/prisma.service';
+import { DiscordService } from './discord/discord.service';
 
 @Module({
   controllers: [ReactionController],
-  providers: [ReactionService, GoogleService, PrismaService],
+  providers: [ReactionService, GoogleService, PrismaService, DiscordService],
 })
 export class ReactionServiceModule {}
