441ff7847de34c12141dfd0ed0aba1908f372486
NicolasAlx <nicolas1.alexandre@epitech.eu>
2024-12-10 20:35:42 +0100

    feat: trigger when a pr or a branch has been created on a github repository has been feated

--- back/prismaDb/seed/2_actions.sql
+++ back/prismaDb/seed/2_actions.sql
@@ -1,6 +1,8 @@
 INSERT INTO "Actions" ("name", "description", "isActive", "createdAt", "serviceId", "body")
 VALUES
     ('check_push_github', 'Action triggered when a user push on a repository.', true, NOW(), (SELECT id FROM "Services" WHERE "name" = 'github' LIMIT 1), '[{"field": "repositoryOwner", "description": "Name of the repository owner "}, {"field": "repositoryName", "description": "Name of the repository"}]'),
+    ('check_new_branch', 'Action triggered when a user create a branch a repository.', true, NOW(), (SELECT id FROM "Services" WHERE "name" = 'github' LIMIT 1), '[{"field": "repositoryOwner", "description": "Name of the repository owner "}, {"field": "repositoryName", "description": "Name of the repository"}]'),
+    ('check_new_pr', 'Action triggered when a user create a pr on a repository.', true, NOW(), (SELECT id FROM "Services" WHERE "name" = 'github' LIMIT 1), '[{"field": "repositoryOwner", "description": "Name of the repository owner "}, {"field": "repositoryName", "description": "Name of the repository"}]'),
 
     ('daily_cron_action', 'Action triggered each day at X hour.', true, NOW(), (SELECT id FROM "Services" WHERE "name" = 'time' LIMIT 1), '[{"field": "hour", "description": "Hour when the cron will be trigered every day (Ex: hour: 10)"}]'),
     ('weekly_cron_action', 'Action triggered each day of the week at X hour.', true, NOW(), (SELECT id FROM "Services" WHERE "name" = 'time' LIMIT 1), '[{"field": "hour", "description": "Hour when the cron will be trigered every day (Ex: hour: 10)"}, {"field": "day", "description": "Day when the cron will be trigered every day (Ex: day: monday OR day: tuesday)"}]'),
