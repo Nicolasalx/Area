90f32e72ec5572a66857c9e15a0768169228c466
NicolasAlx <nicolas1.alexandre@epitech.eu>
2024-12-20 23:50:35 +0100

    fix: units tests have been fixed on the controller and service of the about module

--- back/src/microservices/about-service/about/about.controller.spec.ts
+++ back/src/microservices/about-service/about/about.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AboutController } from './about.controller';
+import { AboutService } from './about.service';
+import { PrismaServiceModule } from '@prismaService/prisma-service.module';
 
 describe('AboutController', () => {
   let controller: AboutController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaServiceModule],
       controllers: [AboutController],
+      providers: [AboutService],
     }).compile();
 
     controller = module.get<AboutController>(AboutController);
--- back/src/microservices/about-service/about/about.service.spec.ts
+++ back/src/microservices/about-service/about/about.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AboutService } from './about.service';
+import { PrismaServiceModule } from '@prismaService/prisma-service.module';
 
 describe('AboutService', () => {
   let service: AboutService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaServiceModule],
       providers: [AboutService],
     }).compile();
 
