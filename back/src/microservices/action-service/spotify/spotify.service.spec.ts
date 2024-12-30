b23bf0227715772b20d9315d79238c941d652186
NicolasAlx <nicolas1.alexandre@epitech.eu>
2024-12-30 16:51:07 +0100

    fix: units tests have been fixed

--- back/src/microservices/action-service/spotify/spotify.service.spec.ts
+++ back/src/microservices/action-service/spotify/spotify.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SpotifyActionService } from './spotify.service';
+import { PrismaServiceModule } from '@prismaService/prisma-service.module';
+import { ActionService } from '@action-service/action/action.service';
 
 describe('SpotifyActionService', () => {
   let service: SpotifyActionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SpotifyActionService],
+      imports: [PrismaServiceModule],
+      providers: [SpotifyActionService, ActionService],
     }).compile();
 
     service = module.get<SpotifyActionService>(SpotifyActionService);
