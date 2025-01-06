f96d564fb1e1c48915bdfb258b95193bb952173b
Manuel Tomé <manuel.tome@epitech.eu>
2025-01-06 11:58:22 +0100

    fix(front): routing after logging

--- front/components/ui/DropdownItem.tsx
+++ front/components/ui/DropdownItem.tsx
@@ -1,29 +1,45 @@
 "use client";
 
 import Link from "next/link";
+import { twMerge } from "tailwind-merge";
 
 interface DropdownItemProps {
   icon?: React.ReactNode;
   children: React.ReactNode;
   onClick?: () => void;
   href?: string;
+  className?: string;
+  textColor?: string;
+  hover?: string;
 }
 
-const DropdownItem = ({ icon, children, onClick, href }: DropdownItemProps) => {
+const DropdownItem = ({ icon, children, onClick, href, className, textColor, hover }: DropdownItemProps) => {
   return href ? (
     <Link
-      className="focus-visible::outline-none flex w-full items-center gap-2 px-4 py-2 text-left text-sm text-gray-700 duration-200 hover:rounded-md hover:bg-gray-100 focus-visible:rounded-md focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2"
+      className={twMerge(
+        "focus-visible::outline-none flex w-full items-center gap-2 px-4 py-2 text-left text-sm text-gray-700 duration-200 hover:rounded-md hover:bg-gray-100 focus-visible:rounded-md focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2",
+        hover,
+        className,
+      )}
       href={href}
     >
-      {icon && <span className="text-gray-500">{icon}</span>}
+      {icon && (
+        <span className={textColor ? "text-gray-500" : textColor}>{icon}</span>
+      )}
       {children}
     </Link>
   ) : (
     <button
-      className="focus-visible::outline-none flex w-full items-center gap-2 px-4 py-2 text-left text-sm text-gray-700 duration-200 hover:rounded-md hover:bg-gray-100 focus-visible:rounded-md focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2"
+      className={twMerge(
+        "focus-visible::outline-none flex w-full items-center gap-2 px-4 py-2 text-left text-sm text-gray-700 duration-200 hover:rounded-md hover:bg-gray-100 focus-visible:rounded-md focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2",
+        hover,
+        className,
+      )}
       onClick={onClick}
     >
-      {icon && <span className="text-gray-500">{icon}</span>}
+      {icon && (
+        <span className={textColor ? "text-gray-500" : textColor}>{icon}</span>
+      )}
       {children}
     </button>
   );
