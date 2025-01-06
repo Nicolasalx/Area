e61f2b21469d539213e796e82a8d608404ffa49a
NicolasMelet <114576883+NicolasMelet@users.noreply.github.com>
2025-01-06 22:52:24 +0100

    feat: my_Area page for mobile (#227)
    * feat: card style almost complete, colored header missing
    
    * feat: header on card
    
    * feat: card header nre now rounded on top left and top right
    
    * feat: svg works, at least trello and google works
    
    * feat: page no areas found done
    
    * merge: main into mobile
    
    * feat: removed service name to clear more space

--- mobile/lib/my_areas.dart
+++ mobile/lib/my_areas.dart
@@ -1,15 +1,17 @@
 import 'dart:convert';
 
+import 'package:area/svg_services.dart';
 import 'package:area/workflow.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:http/http.dart' as http;
 import 'globals.dart' as globals;
+import 'string_extension.dart';
 
 class AreaActions {
   static Future<bool> setActive(bool newState, String workflowId) async {
     try {
-      print("new state: $newState");
       var token = await globals.storage.read(key: "token");
       final response = await http.patch(
         Uri.parse(
@@ -20,10 +22,11 @@ class AreaActions {
         },
         body: json.encode({'isActive': newState}),
       );
-      print(response.body);
+      if (response.statusCode != 200) {
+        return false;
+      }
       return true;
     } catch (error) {
-      print("Unable to communicate with the server");
       return false;
     }
   }
@@ -39,10 +42,11 @@ class AreaActions {
           'Authorization': 'Bearer $token',
         },
       );
-      print(response.body);
+      if (response.statusCode != 200) {
+        return false;
+      }
       return true;
     } catch (error) {
-      print("Unable to communicate with the server");
       return false;
     }
   }
@@ -58,7 +62,7 @@ class AreaActions {
           return SimpleDialog(
             title: const Text(
               'Are you sure you want to delete this area ?',
-              style: const TextStyle(
+              style: TextStyle(
                 fontWeight: FontWeight.w700,
                 fontSize: 20,
               ),
@@ -84,7 +88,7 @@ class AreaActions {
                       child: const Text(
                         'Yes',
                         style: TextStyle(
-                          fontWeight: FontWeight.w700,
+                          color: Colors.red,
                           fontSize: 20,
                         ),
                       ),
@@ -101,7 +105,6 @@ class AreaActions {
                       child: const Text(
                         'No',
                         style: TextStyle(
-                          fontWeight: FontWeight.w700,
                           fontSize: 20,
                         ),
                       ),
@@ -129,123 +132,251 @@ Future<List<Widget>> getWorkflow(
       JsonWorkflowResponse.fromJson(json.decode(response.body));
   List<Widget> widgets = workflow.data.map((item) {
     List<Widget> actions = item.activeActions.map((action) {
-      return Text(
-        action.name,
-        style: const TextStyle(
-          fontWeight: FontWeight.w700,
-          fontSize: 20,
+      return Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.grey[100],
+          ),
+          child: Row(
+            children: [
+              SvgPicture.string(
+                getServiceSvg(action.service.name),
+                width: 20,
+                height: 20,
+              ),
+              Expanded(
+                flex: 8,
+                child: Text(
+                  " ${action.name.format()}",
+                  style: const TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       );
     }).toList();
 
     List<Widget> reactions = item.activeReactions.map((reaction) {
-      return Text(
-        reaction.name,
-        style: const TextStyle(
-          fontWeight: FontWeight.w700,
-          fontSize: 20,
+      return Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.grey[100],
+          ),
+          child: Row(
+            children: [
+              SvgPicture.string(
+                getServiceSvg(reaction.service.name),
+                width: 20,
+                height: 20,
+              ),
+              Expanded(
+                flex: 8,
+                child: Text(
+                  " ${reaction.name.format()}",
+                  style: const TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       );
     }).toList();
 
     return Padding(
       padding: const EdgeInsets.only(top: 20, right: 20),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          color: const Color.fromARGB(255, 255, 255, 255),
-          border: Border.all(
-            color: const Color.fromARGB(255, 185, 185, 185),
-          ),
+      child: Card(
+        semanticContainer: true,
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Text(
-                    item.name,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 23,
+        elevation: 2,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(8.0),
+                  topRight: Radius.circular(8.0),
+                ),
+              ),
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 8,
+                      child: Text(
+                        item.name.format(),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20,
+                        ),
+                      ),
                     ),
+                    const Spacer(),
+                    MaterialButton(
+                      minWidth: 20,
+                      child: Icon(
+                        Icons.power_settings_new,
+                        color: item.isActive ? Colors.green : Colors.black,
+                      ),
+                      onPressed: () {
+                        final newStatus =
+                            AreaActions.setActive(!item.isActive, item.id);
+                        newStatus.then(
+                          (onValue) {
+                            if (onValue) {
+                              callback!();
+                            }
+                          },
+                        );
+                      },
+                    ),
+                    MaterialButton(
+                      minWidth: 20,
+                      child: const Icon(
+                        Icons.delete,
+                        color: Colors.grey,
+                      ),
+                      onPressed: () {
+                        AreaActions._askedDelete(context, item.id, callback);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Row(
+                    children: [
+                      Icon(Icons.play_circle_outlined),
+                      Text(
+                        " Actions",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
                   ),
-                  const Spacer(),
-                  MaterialButton(
-                    minWidth: 40,
-                    child: Icon(
-                      Icons.power_settings_new,
-                      color: item.isActive ? Colors.green : Colors.black,
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: actions,
                     ),
-                    onPressed: () {
-                      final newStatus =
-                          AreaActions.setActive(!item.isActive, item.id);
-                      newStatus.then(
-                        (onValue) {
-                          if (onValue) {
-                            callback!();
-                          }
-                        },
-                      );
-                    },
-                  ),
-                  MaterialButton(
-                    minWidth: 40,
-                    child: const Icon(
-                      Icons.delete,
-                      color: Colors.grey,
-                    ),
-                    onPressed: () {
-                      AreaActions._askedDelete(context, item.id, callback);
-                    },
                   ),
                 ],
               ),
-              const Text(
-                "Actions",
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 20,
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Row(
+                    children: [
+                      Icon(Icons.arrow_forward),
+                      Text(
+                        " Reactions",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: reactions,
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: actions,
-                ),
-              ),
-              const Text(
-                "Reactions",
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 20,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: reactions,
-                ),
-              ),
-              Text(
-                item.isActive ? "Active" : "Not active",
-                style: const TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 23,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }).toList();
 
   return widgets;
+}
+
+class NoAreaFound extends StatelessWidget {
+  const NoAreaFound({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 50,
+        right: 20,
+      ),
+      child: Center(
+        child: Card(
+          elevation: 5,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 30,
+              right: 30,
+              top: 50,
+              bottom: 50,
+            ),
+            child: Column(
+              children: [
+                SvgPicture.string(
+                  '''<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ghost"><path d="M9 10h.01"/><path d="M15 10h.01"/><path d="M12 2a8 8 0 0 0-8 8v12l3-3 2.5 2.5L12 19l2.5 2.5L17 19l3 3V10a8 8 0 0 0-8-8z"/></svg>''',
+                  width: 70,
+                  height: 70,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text(
+                    "No areas found",
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 25),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Text(
+                    "Create your first area to get started",
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 17,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class MyAreasPage extends StatefulWidget {
@@ -267,30 +398,28 @@ class _MyAreasPageState extends State<MyAreasPage> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.only(top: 20.0, left: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "My Areas",
-                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 25),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: FutureBuilder(
-                  future: getWorkflow(rechargePage, context),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<List<Widget>> widgets) {
-                    if (widgets.hasData) {
-                      print(widgets.data!.length);
-                      return Column(children: widgets.data!);
-                    } else {
-                      return const Column();
-                    }
-                  },
-                ),
-              )
-            ],
+          padding: const EdgeInsets.only(
+            top: 20.0,
+            left: 20,
+          ),
+          child: FutureBuilder(
+            future: getWorkflow(rechargePage, context),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Widget>> widgets) {
+              if (widgets.hasData && widgets.data!.isNotEmpty) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: widgets.data!,
+                );
+              } else {
+                return const Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    NoAreaFound(),
+                  ],
+                );
+              }
+            },
           ),
         ),
       ),
