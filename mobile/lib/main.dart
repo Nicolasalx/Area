15450ccef1e65a672cea4920b0324c7ba7b4b194
NicolasMelet <114576883+NicolasMelet@users.noreply.github.com>
2025-01-13 13:06:20 +0100

    feat: can choose server on login (#235)
    * feat: can choose server on login
    
    * fix: reorganize gitignore

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -18,6 +18,7 @@ const routeOAuthDiscord = '/oauth/discord';
 
 Future<void> main() async {
   await dotenv.load(fileName: "lib/.env");
+  await globals.storage.write(key: 'server', value: '10.0.2.2:8080');
   runApp(const MyApp());
 }
 
